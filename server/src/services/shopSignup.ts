db7b46f14a9c6f220cb227cbe86e23fd3d0b93af
Rednull Racing <newproject893420@gmail.com>
2026-09-11 19:51:39 +0900

    add: usecase service getShopSignup5

--- server/src/services/shopSignup.ts
+++ server/src/services/shopSignup.ts
@@ -183,6 +183,60 @@ export const getShopSignup3 = ({ shopSignupId, userId }: UserShopSignupIdParams)
     });
 };
 
+export const getShopSignup5 = ({ shopSignupId, userId }: UserShopSignupIdParams) => {
+    return ShopSignup.findOne({
+        where: {
+            id: shopSignupId,
+            user_id: userId,
+        },
+        attributes: [
+            "id",
+            "company_name",
+            "shop_name",
+            "phone_number",
+            "email",
+            "open_date_time",
+            "founded_date",
+            "member_count",
+            "homepage_url",
+            "company_number",
+            "capital",
+            "auto_trans",
+            "open_info",
+            "user_id",
+        ],
+        include: [
+            {
+                model: ComOrFreeOption,
+            },
+            {
+                model: Name,
+                as: "RepresentativeName",
+                attributes: ["id", "sei", "mei", "sei_kana", "mei_kana"],
+            },
+            {
+                model: Name,
+                as: "ContactName",
+                attributes: ["id", "sei", "mei", "sei_kana", "mei_kana"],
+            },
+            {
+                model: Address,
+                attributes: ["id", "post_number", "shikutyouson", "banchi", "building"],
+                include: [
+                    {
+                        model: TodouhukenOption,
+                        as: "AddressTodouhuken",
+                    },
+                ],
+            },
+            {
+                model: BankAccount,
+                attributes: ["id", "bank_name", "branch_code", "account_number", "meigi", "account_type"],
+            },
+        ],
+    });
+};
+
 export const getMyShopSignupHasS3Data = ({
     shopSignupId,
     userId,
