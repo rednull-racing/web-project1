73bc3eafcdd941db20a9a1a837bc4fd2dd8852de
Rednull Racing <newproject893420@gmail.com>
2026-09-14 16:03:40 +0900

    add: service getShopSignupHasBankAccount

--- server/src/services/shopSignup/query.ts
+++ server/src/services/shopSignup/query.ts
@@ -219,7 +219,19 @@ export const getShopSignup5 = ({ shopSignupId, userId }: UserShopSignupIdParams)
     });
 };
 
-export const getShopSignupHasBankAccount
+export const getShopSignupHasBankAccount = ({ shopSignupId, userId }: UserShopSignupIdParams) => {
+    return ShopSignup.findOne({
+        where: {
+            id: shopSignupId,
+            user_id: userId,
+        },
+        include: [
+            {
+                model: BankAccount,
+            },
+        ],
+    });
+};
 
 export const getMyShopSignupHasS3Data = ({
     shopSignupId,
